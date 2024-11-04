63e0d892f68a746ac944a753c3785af3f82de461
jordanconklin <jordinho@ucla.edu>
2024-11-04 12:13:16 -0800

    setup test and prod env in config

--- config.py
+++ config.py
@@ -1,18 +1,19 @@
-# Create a new file for configuration
+import os
+
+# MONITOR_ENV is set to test in testing environment, but Heroku will have it set to 'prod'
+CURRENT_ENV = os.getenv('MONITOR_ENV')
+
 ENVIRONMENTS = {
     'test': {
         'url': "https://api.github.com/repos/jordanconklin/test/contents/README.md",
-        'check_interval': 20,  # 20 seconds for testing
+        'check_interval': 30,
         'target_url': "https://github.com/jordanconklin/test",
         'description': "Test Repository"
     },
     'prod': {
         'url': "https://api.github.com/repos/SimplifyJobs/New-Grad-Positions/contents/README.md",
-        'check_interval': 60,  # 1 minute for production
+        'check_interval': 60,
         'target_url': "https://github.com/SimplifyJobs/New-Grad-Positions",
         'description': "New Grad Positions"
     }
 }
-
-# Set this to 'test' or 'prod'
-CURRENT_ENV = 'test'
